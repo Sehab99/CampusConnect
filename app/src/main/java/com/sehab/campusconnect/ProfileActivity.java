3d16c109c27915d3e5a02cd37b60917f2c417e43
Sehab <sehabatmain@gmail.com>
2022-06-04 00:54:07 +0530

    - Final bug

--- app/src/main/java/com/sehab/campusconnect/ProfileActivity.java
+++ app/src/main/java/com/sehab/campusconnect/ProfileActivity.java
@@ -1,23 +1,44 @@
 package com.sehab.campusconnect;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+import com.theartofdev.edmodo.cropper.CropImage;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -34,9 +55,13 @@ public class ProfileActivity extends AppCompatActivity {
     private ImageButton chooseImage;
     private static final int PICK_IMAGE_REQUEST = 1;
 
+    ActivityResultLauncher<Intent> cropActivity;
+
     private FirebaseAuth firebaseAuth;
     private DatabaseReference mBase;
-    private Uri uri;
+    String cameraPermission[];
+    String storagePermission[];
+    private Uri imageuri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,13 +80,31 @@ public class ProfileActivity extends AppCompatActivity {
         profilePicture = findViewById(R.id.profile_pic);
         chooseImage = findViewById(R.id.choose_image);
 
+
         firebaseAuth = FirebaseAuth.getInstance();
         mBase = FirebaseDatabase.getInstance().getReference("Users").child(firebaseAuth.getUid());
+
+        cropActivity = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+
+                CropImage.ActivityResult cropResult = CropImage.getActivityResult(result.getData());
+                if (result.getResultCode() == RESULT_OK) {
+                    Uri resultUri = cropResult.getUri();
+                    Picasso.with(ProfileActivity.this).load(resultUri).into(profilePicture);
+
+                }
+            }
+        });
+
         mBase.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 String profileName = snapshot.child("Full Name").getValue().toString();
                 String batch = snapshot.child("Batch").getValue().toString();
+                String url = "";
+                if (snapshot.child("URL").getValue()!=null)
+                    url = snapshot.child("URL").getValue().toString();
                 String departmentName = snapshot.child("Department").getValue().toString();
                 String hostelName = snapshot.child("Hostel").getValue().toString();
                 String regNumber = snapshot.child("RegID").getValue().toString();
@@ -71,10 +114,13 @@ public class ProfileActivity extends AppCompatActivity {
                 textViewBatch.setText(batch);
                 textViewDepartmentName.setText(departmentName);
                 textViewHostelName.setText(hostelName);
+                if (!url.equals(""))
+                Picasso.with(ProfileActivity.this).load(url).into(profilePicture);
                 textViewRegNumber.setText(regNumber);
                 textViewEmailID.setText(emailID);
                 textViewHomeAddress.setText(homeAddress);
             }
+
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
 
@@ -102,10 +148,45 @@ public class ProfileActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null &&
-                data.getData() != null) {
-            uri = data.getData();
-            profilePicture.setImageURI(uri);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null) {
+            imageuri = data.getData();
+            String uid = FirebaseAuth.getInstance().getUid();
+
+
+            // Create a Cloud Storage reference from the app
+            StorageReference storageRef = FirebaseStorage.getInstance().getReference("Users");
+            ProgressDialog dialog = ProgressDialog.show(this,"Please Wait","Uploading the profile");
+            storageRef.child(uid + ".png").putFile(imageuri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                    storageRef.child(uid+".png").getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Uri> task) {
+                            if (task.isSuccessful()) {
+                                String url = task.getResult().toString();
+                                dialog.dismiss();
+                                Picasso.with(ProfileActivity.this).load(imageuri).into(profilePicture);
+                                mBase.child("URL").setValue(url);
+                            }
+                        }
+                    });
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_LONG).show();
+                    dialog.dismiss();
+                }
+            }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @SuppressWarnings("VisibleForTests")
+                @Override
+                public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                    double progress = (100.0 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
+                    dialog.setMessage((int) progress + "% Uploading...");
+                }
+            });
         }
     }
 }
