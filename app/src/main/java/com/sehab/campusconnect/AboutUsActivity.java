f4449719e9515fc4995e184fad0d70d13705264d
Sehab <sehabatmain@gmail.com>
2022-06-10 18:42:07 +0530

    - About us page

--- app/src/main/java/com/sehab/campusconnect/AboutUsActivity.java
+++ app/src/main/java/com/sehab/campusconnect/AboutUsActivity.java
@@ -4,9 +4,13 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class AboutUsActivity extends AppCompatActivity {
+    TextView linkedIn;
+    TextView youTube;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +18,18 @@ public class AboutUsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_about_us);
         getSupportActionBar().setTitle("About us");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        linkedIn = findViewById(R.id.linked_in);
+        youTube = findViewById(R.id.youtube);
+
+        setupHyperlink();
     }
+
+    private void setupHyperlink() {
+        linkedIn.setMovementMethod(LinkMovementMethod.getInstance());
+        youTube.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+
     @Override
     public void onBackPressed() {
         super.onBackPressed();
